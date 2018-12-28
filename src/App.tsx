4b6f1b727dfff413addb5648f622e1874b809db5
Greg Skinner <gregory.skinner@lovehoney.co.uk>
2018-12-28 11:29:24 +0000

    Fix to tslint parsing error in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.scss";
 import logo from "./logo.svg";
 
-class App extends Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="app-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
