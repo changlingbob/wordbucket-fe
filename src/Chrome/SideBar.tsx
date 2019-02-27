a410eb7a601ae45713c269975af6e44b507a53dc
Greg Skinner <gregory.skinner@lovehoney.co.uk>
2019-02-27 16:45:55 +0000

    Dragable sidebar border

--- src/Chrome/SideBar.tsx
+++ src/Chrome/SideBar.tsx
@@ -3,23 +3,71 @@ import Bucket from "wordbucket";
 import Folder from "../Components/Folder/Folder";
 import { BucketState, INavigationState } from "../State/navigation";
 
-export default function SideBar() {
-  return (
-    <BucketState
-      render={(state: INavigationState) => {
-        const container = (
-          <div
-            className ="sidebar"
-          >
-            <Folder
-              state={state}
-              collapsed={false}
-            />
-          </div>
-        );
-
-        return container;
-      }}
-    />
-  );
+interface ISideBarState {
+  width: number;
+  resizeMode: boolean;
+  lastDragX: number;
 }
+
+class SideBar extends React.Component<any, ISideBarState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      lastDragX: 0,
+      resizeMode: false,
+      width: 200,
+    };
+  }
+
+  public render() {
+    const self = this;
+
+    function dragStart(ev: React.MouseEvent<HTMLDivElement>) {
+      if (ev.nativeEvent.offsetX > self.state.width - 10) {
+        document.body.addEventListener("pointermove", dragMove);
+        self.setState({
+          lastDragX: ev.pageX,
+          resizeMode: true,
+        });
+      }
+    }
+
+    function cancelDrag(ev: React.MouseEvent<HTMLDivElement>) {
+      document.body.removeEventListener("pointermove", dragMove);
+      self.setState({resizeMode: false});
+    }
+
+    function dragMove(this: HTMLElement, ev: PointerEvent) {
+      if (self.state.resizeMode && ev.movementX !== 0) {
+        self.setState({
+          lastDragX: ev.pageX,
+          width: self.state.width + (ev.pageX - self.state.lastDragX),
+        });
+      }
+    }
+
+    return (
+      <BucketState
+        render={(state: INavigationState) => {
+          const container = (
+            <div
+              className ="sidebar"
+              onPointerDown={dragStart}
+              onPointerUp={cancelDrag}
+              style={{width: self.state.width}}
+            >
+              <Folder
+                state={state}
+                collapsed={false}
+              />
+            </div>
+          );
+
+          return container;
+        }}
+      />
+    );
+  }
+}
+
+export default SideBar;
