efa893810d2a0be30edd9d5681d935320f08de3e
Greg Skinner <gregory.skinner@lovehoney.co.uk>
2019-03-15 09:25:21 +0000

    !! Broken build !! Needs a shouldComponentUpdate in Folder.tsx to target updates well. Then add creating buckets

--- src/Components/Folder/Folder.tsx
+++ src/Components/Folder/Folder.tsx
@@ -10,7 +10,7 @@ interface IFolderState extends IFolderProps {
 
 interface IFolderProps {
   inPath: (bucketName: string) => boolean;
-  bucket: Bucket;
+  bucket?: Bucket|undefined;
   collapsed: boolean;
   create: boolean;
   path: string;
@@ -22,8 +22,12 @@ class Folder extends React.Component<IFolderProps, IFolderState>  {
     super(props);
     this.state = {
       ...props,
-      collapseChildren: !props.inPath(props.bucket.getName()),
+      collapseChildren: !!props.bucket && !props.inPath(props.bucket.getName()),
     };
+  }
+
+  public shouldComponentUpdate(nextProps: IFolderProps, nextState: IFolderState) {
+    return false;
   }
 
   public componentWillReceiveProps(props: IFolderProps) {
@@ -34,58 +38,75 @@ class Folder extends React.Component<IFolderProps, IFolderState>  {
 
   public render() {
     const bucket = this.state.bucket;
-    const children = [];
-    for (const child of bucket.getChildren()) {
-      children.push(<Folder
-        bucket={child}
-        collapsed={this.state.collapseChildren}
-        inPath={this.state.inPath}
-        path={this.state.path}
-        key={child.getName()}
-        create={false}
-      />);
-    }
+    if (bucket) {
+      const children = [];
+      for (const child of bucket.getChildren()) {
+        children.push(<Folder
+          bucket={child}
+          collapsed={this.state.collapseChildren}
+          inPath={this.state.inPath}
+          path={this.state.path}
+          key={child.getName()}
+          create={false}
+        />);
+      }
+      if (this.state.create) {
+        children.push(<Folder
+          collapsed={this.state.collapseChildren}
+          inPath={this.state.inPath}
+          path={this.state.path}
+          key="test"
+          create={false}
+        />);
+      }
 
-    if (this.state.bucket.getName().length === 0) {
-      return (
-        <>
-          {children}
-        </>
-      );
+      if (bucket.getName().length === 0) {
+        return (
+          <>
+            {children}
+          </>
+        );
+      } else {
+        return (
+          <div
+            className={classNames(
+              "folder",
+              {collapsed: this.props.collapsed},
+              {focused: bucket.getName() === this.state.path},
+            )}
+          >
+            {bucket.getChildren().length > 0 ? <div
+              className="toggle"
+              onClick={() => {
+                this.setState({collapseChildren: !this.state.collapseChildren});
+              }}
+              >
+              {this.state.collapseChildren ? "+" : "-"}
+            </div> : ""}
+            <NavLink
+              className="title"
+              path={bucket.getName()}
+              onClick={() => {
+                this.setState({collapseChildren: false});
+              }}
+            >
+              {bucket.getName()}
+            </NavLink>
+            <div
+              className="create"
+              onClick={() => {
+                this.setState({create: true});
+              }}
+            />
+            <div className="children">
+              {children}
+            </div>
+          </div>
+        );
+      }
     } else {
       return (
-        <div
-          className={classNames(
-            "folder",
-            {collapsed: this.props.collapsed},
-            {focused: bucket.getName() === this.state.path},
-          )}
-        >
-          {this.state.bucket.getChildren().length > 0 ? <div
-            className="toggle"
-            onClick={() => {
-              this.setState({collapseChildren: !this.state.collapseChildren});
-            }}
-            >
-            {this.state.collapseChildren ? "+" : "-"}
-          </div> : ""}
-          <NavLink
-            className="title"
-            path={bucket.getName()}
-            onClick={() => {
-              this.setState({collapseChildren: false});
-            }}
-          >
-            {bucket.getName()}
-          </NavLink>
-          <div
-            className="create"
-            onClick={this.newBucket}
-          />
-          <div className="children">
-            {children}
-          </div>
-        </div>
+        <></>
       );
     }
   }
