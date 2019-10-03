e764abe06b449efc01597e918612cc91ed581a77
Greg Skinner <changling.bob@gmail.com>
2019-10-03 22:26:02 +0100

    We have login! If you have 3rd party cookies enabled.

--- src/Components/Controls/Login.tsx
+++ src/Components/Controls/Login.tsx
@@ -4,10 +4,10 @@ import { BucketState, IApplicationState } from "../../State/state";
 const Login: React.StatelessComponent = () => {
   return (
     <BucketState render={(state: IApplicationState) => {
+
       return (
         <div
           className="g-signin2"
-          data-onsuccess="alert"
         >
           Login
         </div>
--- src/State/googlemanager.ts
+++ src/State/googlemanager.ts
@@ -1,12 +1,9 @@
 class GoogleManager {
   public GoogleAuth?: gapi.auth2.GoogleAuth;
+  // private readonly platformUrl: string = "https://apis.google.com/js/platform.js";
   private readonly gapiUrl: string = "https://apis.google.com/js/platform.js";
-  private authPromise: Promise<any>;
-  private authResolve: () => void;
 
   constructor() {
-    // tslint:disable-next-line:no-empty
-    this.authResolve = () => {};
     const self = this;
 
     const metaTag = document.createElement("meta");
@@ -20,49 +17,23 @@ class GoogleManager {
     apiElement.charset = "utf-8";
     document.body.appendChild(apiElement);
     apiElement.onload = () => {
-      gapi.load("client", initGapi);
+      gapi.load("auth2", initGapi);
     };
 
     function initGapi() {
-      // gapi.client.init({
-      //   clientId: "404024621165-t0sbcvfkac2m8u4b8l3p04hm9r2jtqcg.apps.googleusercontent.com",
-      //   discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
-      //   scope: "https://www.googleapis.com/auth/drive.appdata",
-      // }).then(() => {
-      //   self.GoogleAuth = gapi.auth2.getAuthInstance();
-      //   self.GoogleAuth.isSignedIn.listen(self.updateSigninStatus);
-      // }, () => {
-      //   alert("you need to done do cookies");
-      // });
-    }
-
-    this.authPromise = new Promise((resolve, reject) => {
-      self.authResolve = resolve;
-    });
-  }
-
-  public signIn = () => {
-    if (this.GoogleAuth) {
-      this.GoogleAuth.signIn();
-    }
-  }
-
-  public signOut = () => {
-    if (this.GoogleAuth) {
-      this.GoogleAuth.signOut();
+      gapi.auth2.getAuthInstance().then((auth) => {
+        self.GoogleAuth = auth;
+      }, (err) => {
+        alert(JSON.stringify(err));
+        throw err;
+      });
     }
   }
 
   public save(data: string) {
-    this.authPromise.then();
   }
 
-  private updateSigninStatus(isSignedIn: boolean) {
-    if (isSignedIn) {
-      // tslint:disable-next-line:no-console
-      console.log("success");
-      this.authResolve();
-    }
+  public load(data: string) {
   }
 }
 
