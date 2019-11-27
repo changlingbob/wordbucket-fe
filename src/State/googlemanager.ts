d7648a7a0977c3fd434d617f8cc2a0bf9a080474
Greg Skinner <gregory.skinner@lovehoney.co.uk>
2019-11-27 15:45:28 +0000

    Loading is now a magical array of files

--- src/State/googlemanager.ts
+++ src/State/googlemanager.ts
@@ -1,6 +1,18 @@
+import Bucket from "wordbucket/out/Bucket";
+
+// Figuring out how do to this was a pain. It critically doesn't use an NPM module
+// because there isn't one that does the right stuff properly that I could find.
+// We're loading the Google OAuth via just chucking it in a script tag and a magic
+// css class, but then using a gloablly registered name to load. The documentation
+// is mostly reading npm:gdrive-appdata and hoping it's accurate.
+
+interface IFilenameMap {
+  [key: string]: string;
+}
+
 class GoogleManager {
   private GoogleAuth?: gapi.auth2.GoogleAuth;
-  private fileIds: string[] = [];
+  private fileIds: IFilenameMap = {};
   private fileName: string = "wordbucket.json";
   private clientId: string = "404024621165-t0sbcvfkac2m8u4b8l3p04hm9r2jtqcg.apps.googleusercontent.com";
   private loadBucket: (bucketString: string) => void;
@@ -43,58 +55,77 @@ class GoogleManager {
     };
   }
 
-  public save = async (data: string) => {
+  public save = async (data: Bucket) => {
     if (this.GoogleAuth) {
-
+      // gapi.client.drive.files.;
     }
   }
 
   public load = async () => {
-    if (this.fileIds) {
-      gapi.client.drive.files.get({
-        alt: "media",
-        fileId: this.fileIds[0],
-      }).then((res) => {
-        console.log(`got file:`);
-        console.log(res);
+    console.log("load function");
+    if (Object.keys(this.fileIds).length > 0) {
+      Promise.all(
+        Object.keys(this.fileIds).map(
+          (fileName) => {
+            return gapi.client.drive.files.get({
+              alt: "media",
+              fileId: this.fileIds[fileName],
+            }).then((res) => {
+              console.log(`got file:`);
+              console.log(res);
+              return res;
+            });
+          },
+        ),
+      ).then((files) => {
+        console.log("promise has returned");
+        console.log(files);
       });
     } else {
-      this.getFileIds().then(this.load);
+      this.getFileIds().then((ids) => {if (Object.keys(ids).length > 0) {this.load(); }});
     }
   }
 
-  private create = async (): Promise<string[]> => {
+  private create = async (fileName: string): Promise<IFilenameMap> => {
+    console.log("create function");
     return gapi.client.drive.files.create({
       fields: "id",
-      resource: { name: this.fileName, parents: ["appDataFolder"] },
+      resource: { name: fileName, parents: ["appDataFolder"] },
     }).then((response) => {
       if (response
         && response.result
         && response.result.id
       ) {
-        this.fileIds.push(response.result.id);
+        this.fileIds[fileName] = response.result.id;
       }
       return this.fileIds;
     });
   }
 
-  private getFileIds = async (): Promise<string[]> => {
+  private getFileIds = async (): Promise<IFilenameMap> => {
+    console.log("getFileIds function");
     return gapi.client.drive.files.list({
-      fields: "files(id)",
-      q: `name="${this.fileName}"`,
+      fields: "files(name, id)",
       spaces: "appDataFolder",
     }).then((response) => {
       if (response
         && response.result
         && response.result.files
-        && response.result.files[0]
-        && response.result.files[0].id
+        && response.result.files.length > 0
       ) {
-        this.fileIds = response.result.files.map((file) => file.id || "");
-        return this.fileIds;
-      } else {
-        return this.create();
+        const newMap: IFilenameMap = {};
+        console.log(response);
+        response.result.files.forEach(
+          (file) => {
+            if (file.name && file.id) {
+              newMap[file.name] = file.id;
+            }
+          },
+        );
+
+        this.fileIds = newMap;
       }
+      return this.fileIds;
     });
   }
 }
