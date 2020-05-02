f81b1ee5b6633943d07cee9ce73e8e3cc96d9c38
Greg Skinner <changling.bob@gmail.com>
2020-05-02 22:32:40 +0100

    Updates

---
+++ precache-manifest.409cd81764be533a3c572c1e846ebc86.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5585188bbdd9f0088020160abb8bb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad83f9f5af84b34f2b0c",
+    "url": "/static/css/2.0e433876.chunk.css"
+  },
+  {
+    "revision": "d0b3701d247feb8f406f",
+    "url": "/static/css/main.609c89a9.chunk.css"
+  },
+  {
+    "revision": "ad83f9f5af84b34f2b0c",
+    "url": "/static/js/2.9d66de9d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9d66de9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b3701d247feb8f406f",
+    "url": "/static/js/main.48e4ccfd.chunk.js"
+  },
+  {
+    "revision": "f6e36d06fcf3963b9845",
+    "url": "/static/js/runtime-main.cc827bad.js"
+  },
+  {
+    "revision": "9921f67c23198cb4c85f005c7e56a882",
+    "url": "/static/media/add-new-document.9921f67c.svg"
+  },
+  {
+    "revision": "c6144ed4a4dc15f5ed80d7b91e2b1a19",
+    "url": "/static/media/add.c6144ed4.svg"
+  },
+  {
+    "revision": "590dc343470c32c0e3f7f2b1aa2f18bc",
+    "url": "/static/media/delete-page.590dc343.svg"
+  },
+  {
+    "revision": "6a0c831bc94c2115b4cb85170f30dbf8",
+    "url": "/static/media/role-game.6a0c831b.svg"
+  }
+]);
